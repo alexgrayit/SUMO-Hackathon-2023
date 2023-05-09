89f18068966ea073b48df93537da37210316c0b1
Alex Gray <alex@symbioticconsulting.com.au>
2023-05-10 00:13:23 +1000

    Mostly completed problem 1
    Up to plotting and animating.

--- Problem1.py
+++ Problem1.py
@@ -1,20 +1,112 @@
-input_file = "input_p1.txt"
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('TkAgg')  # for displaying in separate window
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Computer Modern Roman"
+})
+
+# Taken from https://www.geeksforgeeks.org/program-find-gcd-floating-point-numbers/
+# Recursive function to return gcd
+# of a and b
+def gcd(a, b):
+    if (a < b):
+        return gcd(b, a)
+
+    # base case
+    if (abs(b) < 0.001):
+        return a
+    else:
+        return (gcd(b, a - np.floor(a / b) * b))
+
+
+class Sensor:
+    times = []
+    location = [0, 0]
+
+    def __init__(self, location, times):
+        self.times = times
+        self.location = location
+
+inputFile = "input_p1.txt"
 try: 
-    f = open(input_file,"r")
-    input_lines = []
-    
-    sensorsInFile = -1
-    for line in f:
-        sensorsInFile += 1
-        input_lines.append(line.strip())
+    f = open(inputFile, "r")
 
-    lapsStr,sensorsStr = input_lines[0].split(", ")
-    laps = int(lapsStr)
-    sensors = int(sensorsStr)
-
-    if(sensors != sensorsInFile):
-        print("Not enough sensors provided")
-
-    f.close()
 except FileNotFoundError:
     print("File not found")
+    exit(-1)
+
+inputLines = []
+
+sensorsInFile = -1
+for line in f:
+    sensorsInFile += 1
+    inputLines.append(line.strip())
+try:
+    lapsStr, sensorsStr = inputLines[0].split(", ")
+    laps = int(lapsStr)
+    sensors = int(sensorsStr)
+except:
+    print("There was an error in the first line, invalid format")
+    exit(-1)
+
+if sensors != sensorsInFile:
+    print("Not enough sensors provided")
+
+# Collect sensor objects and times into arrays
+sensorLines = inputLines[1:]
+sensors = []
+timesMatrix = []
+
+timeLen = 0
+
+for i in range(1, len(sensorLines)):
+    splitLine = inputLines[i].split(', ')
+    sensorX = splitLine[0]
+    sensorY = splitLine[1]
+    pos = np.array([sensorX, sensorY]).astype(float)
+
+    sensorMeasurements = np.array(splitLine[2:]).astype(float)
+    if i == 1:
+        timeLen = len(sensorMeasurements)
+    else:
+        if len(sensorMeasurements) != timeLen:
+            print("Number of measurements from each sensor is different.")
+            exit(-1)
+
+    sensor = Sensor(pos, sensorMeasurements)
+    sensors.append(sensor)
+    timesMatrix.append(sensorMeasurements)
+
+# Transpose times so that times [[0 -> n], [...], ...] are all in consecutive order
+timesMatrix = np.array(timesMatrix)
+timesMatrix = timesMatrix.transpose()
+
+# Check the sensor times are in the correct order
+tPrev = 0
+for ti in range(len(timesMatrix)):
+    for tj in range(len(timesMatrix[ti])):
+        if timesMatrix[ti][tj] < tPrev:
+            if timesMatrix[ti][tj] < 0:
+                print("There is an error in the times, a time is negative.")
+            else:
+                print("Times are out of order from the sensor order.")
+            exit(-1)
+        else:
+            tPrev = timesMatrix[ti][tj]
+
+sensorPlotPoints = []
+for sensor in sensors:
+    sensorPlotPoints.append(sensor.location)
+sensorPlotPoints.append(sensors[0].location)
+
+sensorPlotPoints = np.array(sensorPlotPoints).transpose()
+
+plt.plot(sensorPlotPoints[0], sensorPlotPoints[1])
+plt.show()
+
+f.close()
+# f = open(input_file, "r")
+# print(f.read())
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
+<script type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_CHTML">
+</script>
+<script type="text/x-mathjax-config">
+  MathJax.Hub.Config({
+    tex2jax: {
+      inlineMath: [['$','$'], ['\\(','\\)']],
+      processEscapes: true},
+      jax: ["input/TeX","input/MathML","input/AsciiMath","output/CommonHTML"],
+      extensions: ["tex2jax.js","mml2jax.js","asciimath2jax.js","MathMenu.js","MathZoom.js","AssistiveMML.js", "[Contrib]/a11y/accessibility-menu.js"],
+      TeX: {
+      extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"],
+      equationNumbers: {
+      autoNumber: "AMS"
+      }
+    }
+  });
+</script>
+
 # SUMO Hackathon 2023
+
+## Problem 1
+### Definitions and Parameters
+For this problem we define that the input file will be in the form
+```
+<N_laps>, <n_sensors>
+<x_1>, <y_1>, <t_1>, <t_2>,... , <t_N>
+<x_2>, <y_2>, <t_1>, <t_2>,... , <t_N>
+.
+.
+.
+<x_n>, <y_n>, <t_1>, <t_2>,... , <t_N>
+```
+Where the first line defines the number of laps (measurements made by each sensor) each subsequent
+line corresponds to sensor $n$ and has an $$x$$ and a $$y$$ coordinate for that sensor as its first two items,
+and then the following $$N$$ items are the times at which the car passed that sensor (in chronological order).
